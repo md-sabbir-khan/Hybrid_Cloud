49cc7d85b43410cb2333d51720532f71b1418c36
MD SABBIR KHAN <64284482+md-sabbir-khan@users.noreply.github.com>
2020-06-15 12:36:47 +0530

    first

---
+++ idex.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Testing Site</title>
+    </head>
+    <body>
+        <p> <marquee behavior="scroll" direction="right">Thanks for Enlighment us our inspiration, indian one man army World 1st record holder Vimal DAga sir..</marquee></p>
+            <?php
+
+            <img src="https://my_webdata@bucket.s3.ap-south-1.amazonaws.com/vimal sir.jpg", height='256', width='256'>
+
+            ?>
+        
+    </body>
+</html> 
+
