f5b92981465da0bd1e86b540a2935ba95debf025
MD SABBIR KHAN <64284482+md-sabbir-khan@users.noreply.github.com>
2020-06-15 13:44:18 +0530

    second " change bucket"

--- idex.php
+++ idex.php
@@ -8,7 +8,7 @@
         <p> <marquee behavior="scroll" direction="right">Thanks for Enlighment us our inspiration, indian one man army World 1st record holder Vimal DAga sir..</marquee></p>
             <?php
 
-            <img src="https://my_webdata@bucket.s3.ap-south-1.amazonaws.com/vimal sir.jpg", height='256', width='256'>
+            <img src="https://terratest001-bucket.s3.ap-south-1.amazonaws.com/vimal sir.jpg", height='256', width='256'>
 
             ?>
         
